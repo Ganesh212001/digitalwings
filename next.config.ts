a93499a37fdf61cc4543f2128e0ecf18d5f1e296
Ganesh212001 <ganeshmahajan21062001@gmail.com>
2024-11-25 10:25:23 +0530

    ignore build

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ['flowbite.s3.amazonaws.com','images.unsplash.com'],
-  }
+  },
+  reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
